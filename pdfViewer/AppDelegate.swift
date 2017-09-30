c42b45b49fef6ee2f4d90bff7e1201447c9ce224
Alex Lesa <alexlesa@dn-wlan-133-040.sns.otago.ac.nz>
2017-09-30 13:16:22 +1300

    Initial Commit

---
+++ pdfViewer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  pdfViewer
+//
+//  Created by Alex Lesa on 30/09/17.
+//  Copyright © 2017 Alex Lesa. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ pdfViewer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  pdfViewer
+//
+//  Created by Alex Lesa on 30/09/17.
+//  Copyright © 2017 Alex Lesa. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
